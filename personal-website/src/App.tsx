aee04863f3eaf0b5bda1451d8e580b25d5ab67a6
Caitlyn.Kloeckl <caitlyn.kloeckl@code42.com>
2021-10-12 18:02:33 -0500

    work on another device

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,9 +1,8 @@
-import './App.css';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-import profileImage from './profile-image.jpg';
-
+import "./App.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import profileImage from "./profile-image.jpg";
 
 function App() {
   return (
@@ -11,39 +10,89 @@ function App() {
       <Navbar>
         <Container className="navbar">
           <Nav>
-            <Nav.Link className="navbar-text" href="#home">Home</Nav.Link>
-            <Nav.Link className="navbar-text" href="#about">About</Nav.Link>
-            <Nav.Link className="navbar-text" href="#resume">Resume</Nav.Link>
-            <Nav.Link className="navbar-text" href="#resume">Projects</Nav.Link>
+            <Nav.Link className="navbar-text" href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="navbar-text" href="#about">
+              About
+            </Nav.Link>
+            <Nav.Link className="navbar-text" href="#resume">
+              Resume
+            </Nav.Link>
+            <Nav.Link className="navbar-text" href="#resume">
+              Projects
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
       <header id="home" className="title-section">
-        <h1 className="main-title">
-          Caitlyn Kloeckl
-      </h1>
-        <h4>
-          Software Engineer, Physics Major, Photographer, Plant Lover. </h4>
+        <h1 className="main-title">Caitlyn Kloeckl</h1>
+        <h4>Software Engineer, Physics Major, Photographer, Plant Lover. </h4>
         <hr className="line"></hr>
         <div className="col-lg-8">
-          <div style={{ display: "inline-block" , paddingLeft:"20px", paddingTop: "20px", paddingBottom: "20px", paddingRight:"20px"}}>
-            <a href="https://www.linkedin.com/in/caitlyn-kloeckl/" target="_blank" className="social-buttons"><i style={{ paddingRight: "20px" }} className="fa fa-linkedin"></i>LinkedIn</a>
+          <div
+            style={{
+              display: "inline-block",
+              paddingLeft: "20px",
+              paddingTop: "20px",
+              paddingBottom: "20px",
+              paddingRight: "20px",
+            }}
+          >
+            <a
+              href="https://www.linkedin.com/in/caitlyn-kloeckl/"
+              target="_blank"
+              className="social-buttons"
+            >
+              <i
+                style={{ paddingRight: "20px" }}
+                className="fa fa-linkedin"
+              ></i>
+              LinkedIn
+            </a>
           </div>
-          <div style={{ display: "inline-block" , paddingLeft:"20px", paddingTop: "20px", paddingBottom: "20px", paddingRight:"20px"}}>
-            <a href="https://www.caitlynkloecklphotography.com/" target="_blank" className="social-buttons"><i style={{ paddingRight: "20px" }} className="fa fa-camera"></i>Photography</a>
+          <div
+            style={{
+              display: "inline-block",
+              paddingLeft: "20px",
+              paddingTop: "20px",
+              paddingBottom: "20px",
+              paddingRight: "20px",
+            }}
+          >
+            <a
+              href="https://www.caitlynkloecklphotography.com/"
+              target="_blank"
+              className="social-buttons"
+            >
+              <i style={{ paddingRight: "20px" }} className="fa fa-camera"></i>
+              Photography
+            </a>
           </div>
         </div>
       </header>
       <section id="about" className="about-section">
         <div className="container">
           <div className="row">
-            <div style={{ paddingTop: "30px", paddingBottom: "30px" }} className=" col-lg-4 col-sm-1">
+            <div
+              style={{ paddingTop: "30px", paddingBottom: "30px" }}
+              className=" col-lg-4 col-sm-1"
+            >
               <img src={profileImage} width="250" height="350" />
             </div>
             <div className="col-lg-8 text-start">
               <div>
                 <h2>About Me</h2>
-                <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="about-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
               </div>
               <div>
                 <hr></hr>
@@ -54,23 +103,46 @@ function App() {
                 </p>
               </div>
               <div style={{ paddingTop: "20px", paddingBottom: "30px" }}>
-                <a style={{ width: "260px" }} className="social-buttons" href="https://github.com/Caitlyn-Kloeckl/resume-documents/raw/main/Resume.pdf"><i style={{ paddingRight: "20px" }} className="fa fa-download"></i>Download Resume</a>
+                <a
+                  style={{ width: "260px" }}
+                  className="social-buttons"
+                  href="https://github.com/Caitlyn-Kloeckl/resume-documents/raw/main/Resume.pdf"
+                >
+                  <i
+                    style={{ paddingRight: "20px" }}
+                    className="fa fa-download"
+                  ></i>
+                  Download Resume
+                </a>
               </div>
             </div>
           </div>
         </div>
       </section>
       <section id="resume" className="resume-section">
-        <div style={{ paddingBottom: "25px" }} className="container">
+        <div style={{ paddingBottom: "25px", paddingTop: "25px" }} className="container">
           <div className="row">
             <div className="col-lg-4 col-sm-1">
               <h2 className="resume-main-titles">Education</h2>
             </div>
             <div className="col-lg-7 text-start">
               <div>
-                <h3 className="resume-secondary-titles">University of Minnesota - College of Science and Engineering</h3>
-                <p className="resume-tertiary-titles"><i>Bachelor of Science</i> • Expected December 2021</p>
-                <p className="resume-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h3 className="resume-secondary-titles">
+                  University of Minnesota - College of Science and Engineering
+                </h3>
+                <p className="resume-tertiary-titles">
+                  <i>Bachelor of Science</i> • Expected December 2021
+                </p>
+                <p className="resume-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
               </div>
             </div>
           </div>
@@ -79,16 +151,60 @@ function App() {
         <div style={{ paddingBottom: "25px" }} className="container">
           <div className="row">
             <div className="col-lg-4 col-sm-1">
-              <h2 className="resume-main-titles">Work <br></br>Experience</h2>
+              <h2 className="resume-main-titles">
+                Work <br></br>Experience
+              </h2>
             </div>
             <div className="col-lg-7 text-start">
               <div>
-                <h3 className="resume-secondary-titles">Technology Help Specialist</h3>
-                <p className="resume-tertiary-titles"><i>Univerisy of Minnesota IT</i> • October 2019 – June 2021</p>
-                <p className="resume-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <h3 className="resume-secondary-titles">Photographer - Self Employed</h3>
-                <p className="resume-tertiary-titles"><i>Univerisy of Minnesota IT</i> • September 2020 – Current</p>
-                <p className="resume-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <h3 className="resume-secondary-titles">
+                  QA Automation Intern
+                </h3>
+                <p className="resume-tertiary-titles">
+                  <i>Code42</i> • June 2019 – Current
+                </p>
+                <p className="resume-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+                <h3 className="resume-secondary-titles">
+                  Technology Help Specialist
+                </h3>
+                <p className="resume-tertiary-titles">
+                  <i>University of Minnesota IT</i> • October 2019 – June 2021
+                </p>
+                <p className="resume-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+                <h3 className="resume-secondary-titles">
+                  Photographer - Self Employed
+                </h3>
+                <p className="resume-tertiary-titles">
+                  <i>Minnesota </i> • September 2020 – Current
+                </p>
+                <p className="resume-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
               </div>
             </div>
           </div>
@@ -104,32 +220,68 @@ function App() {
                 <div style={{ paddingLeft: "0px" }} className="container">
                   <div className="skills-bar">
                     <div className="bar">
-                      <div className="info">
-                        HTML
+                      <div className="info">ANGULAR</div>
+                      <div className="progress-line">
+                        <span className="angular"></span>
                       </div>
-                      <div className="progress-line"><span className="html"></span></div>
                       <div className="bar">
-                        <div className="info">
-                          <span>CSS</span>
+                        <div className="info">REACT</div>
+                        <div className="progress-line">
+                          <span className="react"></span>
                         </div>
-                        <div className="progress-line"><span className="css"></span></div>
                         <div className="bar">
                           <div className="info">
-                            <span>BOOTSTRAP</span>
+                            <span>HTML</span>
                           </div>
-                          <div className="progress-line"><span className="bootstrap"></span></div>
+                          <div className="progress-line">
+                            <span className="html"></span>
+                          </div>
                           <div className="bar">
                             <div className="info">
-                              <span>JAVASCRIPT</span>
+                              <span>CSS</span>
                             </div>
                             <div className="progress-line">
-                              <span className="javascript"></span>
+                              <span className="css"></span>
                             </div>
                             <div className="bar">
                               <div className="info">
-                                <span>C Programming</span>
+                                <span>BOOTSTRAP</span>
                               </div>
-                              <div className="progress-line"><span className="c"></span></div>
+                              <div className="progress-line">
+                                <span className="bootstrap"></span>
+                              </div>
+                              <div className="bar">
+                                <div className="info">
+                                  <span>TEST AUTOMATION</span>
+                                </div>
+                                <div className="progress-line">
+                                  <span className="test-automation"></span>
+                                </div>
+                                <div className="bar">
+                                  <div className="info">
+                                    <span>PYTHON</span>
+                                  </div>
+                                  <div className="progress-line">
+                                    <span className="python"></span>
+                                  </div>
+                                  <div className="bar">
+                                    <div className="info">
+                                      <span>JAVA</span>
+                                    </div>
+                                    <div className="progress-line">
+                                      <span className="java"></span>
+                                    </div>
+                                    <div className="bar">
+                                      <div className="info">
+                                        <span>MATLAB</span>
+                                      </div>
+                                      <div className="progress-line">
+                                        <span className="matlab"></span>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
@@ -142,8 +294,7 @@ function App() {
           </div>
         </div>
       </section>
-      <section id="projects" className="projects-section">
-      </section>
+      <section id="projects" className="projects-section"></section>
     </div>
   );
 }
